2c3fe6fe60e973740da00b9220cd9cc35afb3c0a
lucas <lucas.garciasj@gmail.com>
2021-01-16 09:52:54 +0100

    Test

---
+++ lib/Prescription-Info-Input.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main () {
+  print("Patient Name: ");
+  String patientname = stdin.readLineSync();
+
+
+  int Day;
+  int Month;
+  int Year;
+  String Date;
+
+  Date = (Day"," Month"," Year);
+  print("Day: ");
+  Day = stdin.readByteSync();
+  print("Month: ");
+  Month = stdin.readByteSync();
+  print("Year: ");
+  Year = stdin.readByteSync();
+
+  print(Date);
+
+}
